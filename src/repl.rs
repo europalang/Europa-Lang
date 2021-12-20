8b01ff481c24a8d6665146cc320ccaa20d61cfba
Patrick Winters <19wintersp@gmail.com>
2021-12-20 15:44:54 +0000

    Add readline and bracket-pairing for REPL

--- src/repl.rs
+++ src/repl.rs
@@ -1,44 +1,145 @@
-use std::io::{stdin, stdout, Write};
 use std::process;
+use std::time::Instant;
 
 use crate::environment::Environment;
+use crate::error::{Error, LineInfo};
+use crate::interpreter::Interpreter;
+use crate::lexer::Lexer;
+use crate::parser::Parser;
+use crate::resolver::Resolver;
+use crate::token::{Token, TType};
 use crate::types::Type;
 
-use super::run_string;
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
 
 pub fn init(mut environ: Environment, verbose: bool) {
-    loop {
-        // Same line print
-        print!("\x1b[33m>\x1b[0m ");
-        stdout().flush().unwrap();
+    let mut code; let mut tokens;
+    let mut rl = Editor::<()>::new();
 
-        // Wait for input from user
-        let mut input = String::new();
-        match stdin().read_line(&mut input) {
-            Err(e) => {
-                println!("Unexpected REPL Error: {:?}", e);
-                process::exit(1);
+    'main_loop: loop {
+        tokens = Vec::new();
+        code = String::new();
+
+        let mut line = 1;
+
+        while tokens.len() == 0 || has_unclosed_brackets(&tokens) {
+            let prompt = match tokens.len() {
+                0 => "> ",
+                _ => {
+                    tokens.pop().unwrap(); // remove the EOF
+                    "... "
+                },
+            };
+
+            let read = match rl.readline(prompt) {
+                Ok(read) => read,
+                Err(ReadlineError::Eof) => break 'main_loop,
+                Err(ReadlineError::Interrupted) => continue 'main_loop,
+                Err(err) => {
+                    eprintln!("Unexpected error: {}", err);
+                    process::exit(1);
+                },
+            };
+
+            if read == ".exit" {
+                break 'main_loop
             }
-            Ok(n) => {
-                if n == 0 {
-                    println!("\n");
-                    process::exit(0);
-                }
-                input = input.trim().to_string();
+
+            code.push_str(&read);
+            code.push('\n');
+
+            let mut lexer = Lexer::new(&read);
+            lexer.set_lineinfo(LineInfo::new(line, 0));
+            match lexer.init() {
+                Ok(mut lexed) => tokens.append(&mut lexed),
+                Err(error) => {
+                    error.display(&read);
+                    continue 'main_loop
+                },
             }
+
+            line += 1;
         }
 
-        // Exit out of program
-        if input.eq("exit") {
-            process::exit(0);
-        }
-
-        // Attempt to run code
-        match run_string(&input, &mut environ, verbose) {
-            Err(e) => e.display(&input),
+        match run_code(&tokens, &mut environ, verbose) {
+            Err(error) => error.display(&code),
             Ok(eval) => if eval != Type::Nil {
-                println!("{}", eval)
+                println!("{}", eval);
             },
-        };
+        }
     }
+}
+
+fn run_code(
+    code: &[Token],
+    environ: &mut Environment,
+    verbose: bool,
+) -> Result<Type, Error> {
+    // Turn tokens into AST
+    let mut time = Instant::now();
+    let tree = Parser::new(code.to_vec()).init()?;
+
+    if verbose {
+        eprintln!("parser {:?}", time.elapsed());
+    }
+
+    // Create interpreter
+    let mut interpreter = Interpreter::new(tree, environ.clone());
+
+    // Resolve variables
+    time = Instant::now();
+    interpreter = Resolver::new(interpreter).init()?;
+
+    if verbose {
+        eprintln!("resolver {:?}", time.elapsed());
+    }
+
+    // Run interpreter
+    time = Instant::now();
+    let eval = interpreter.init()?;
+
+    if verbose {
+        eprintln!("interpreter {:?}", time.elapsed());
+    }
+
+    *environ = interpreter.environ;
+
+    Ok(eval)
+}
+
+fn has_unclosed_brackets(code: &[Token]) -> bool {
+    #[derive(Clone, Copy, PartialEq)]
+    enum BracketType {
+        BBrace,
+        Brace,
+        Paren,
+        Brack,
+    }
+
+    let mut stack = Vec::new();
+
+    for token in code {
+        match token.ttype {
+            TType::LeftBBrace => stack.push(BracketType::BBrace),
+            TType::LeftBrace => stack.push(BracketType::Brace),
+            TType::LeftParen => stack.push(BracketType::Paren),
+            TType::LeftBrack => stack.push(BracketType::Brack),
+            TType::RightBBrace => if stack.pop() != Some(BracketType::BBrace) {
+                return false
+            },
+            TType::RightBrace => if stack.pop() != Some(BracketType::Brace) {
+                return false
+            },
+            TType::RightParen => if stack.pop() != Some(BracketType::Paren) {
+                return false
+            },
+            TType::RightBrack => if stack.pop() != Some(BracketType::Brack) {
+                return false
+            },
+            _ => (),
+        }
+    }
+
+    stack.len() > 0
 }
