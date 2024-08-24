576ffe0fdef987f40a4fe3d06c76566ff919787b
michael-m-2983 <michaelmccright2983@gmail.com>
2024-08-23 22:13:37 -0500

    Add clock.fmt library function

--- src/stdlib/clock.rs
+++ src/stdlib/clock.rs
@@ -1,7 +1,8 @@
 use std::{
     rc::Rc,
-    time::{SystemTime, UNIX_EPOCH},
+    time::{SystemTime, UNIX_EPOCH, Duration},
 };
+use chrono::{DateTime, Utc};
 
 use maplit::hashmap;
 
@@ -19,7 +20,20 @@ pub fn new() -> Module {
             "now".into() => native_func!(|_, _, _| {
                 let start = SystemTime::now().duration_since(UNIX_EPOCH).expect("Error getting time.");
                 Ok(Type::Float(start.as_millis() as f32))
-            }, 0)
+            }, 0),
+            "fmt".into() => native_func!(|_, args, _| {
+
+                let millis: f32 = match args[0] {
+                    Type::Float(value) => value,
+                    _ => {
+                        eprintln!("Invalid argument passed to clock.fmt!");
+                        0.0
+                    }
+                };
+                let datetime: DateTime<Utc> = (UNIX_EPOCH + Duration::from_millis(millis as u64)).into();
+
+                Ok(Type::String(datetime.format(args[1].to_string().as_str()).to_string()))
+            }, 2)
         },
     }
 }
